1309718d52c02a46ec8d89862ab5ab7f15d44700
KYW <qodna25@gmail.com>
2024-03-19 21:53:49 +0900

    deploy: 호스팅 환경 호환성 체크 후 재배포

--- dist/src/bin/www.js
+++ dist/src/bin/www.js
@@ -1,5 +1,5 @@
 "use strict";
-const app = require('../server.js');
+const app = require('../web.js');
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
     console.log("포트:", PORT + ' 가동 중...');
